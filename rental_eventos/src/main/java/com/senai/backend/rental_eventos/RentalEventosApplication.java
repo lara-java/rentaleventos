4628200c2cb3d5732643690cf825da03ee945a65
laramartinx <26.laracmartins@gmail.com>
2026-09-01 12:01:56 -0300

    update

--- rental_eventos/src/main/java/com/senai/backend/rental_eventos/RentalEventosApplication.java
+++ rental_eventos/src/main/java/com/senai/backend/rental_eventos/RentalEventosApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RentalEventosApplication {
 
+	//testando
 	public static void main(String[] args) {
 		SpringApplication.run(RentalEventosApplication.class, args);
 	}
